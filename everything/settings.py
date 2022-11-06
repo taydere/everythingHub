5a602eac88877c98b4824c416e52b15864b5c09a
Taylor Dere <taylordere@gmail.com>
2022-11-06 14:02:13 -0800

    database

--- everything/settings.py
+++ everything/settings.py
@@ -82,14 +82,21 @@ WSGI_APPLICATION = 'everything.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+   # 'default': {
+       # 'ENGINE': 'django.db.backends.postgresql',
+      #  'NAME': 'railway',
+     #   'USER': 'postgres',
+    #    'PASSWORD': 'nuOrhBiLwcakp0iUIRWD',
+   #     'HOST': 'containers-us-west-106.railway.app',
+  #      'PORT': '7431',
+ #   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'nuOrhBiLwcakp0iUIRWD',
-        'HOST': 'containers-us-west-106.railway.app',
-        'PORT': '7431',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
